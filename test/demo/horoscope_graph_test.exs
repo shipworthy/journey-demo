2f251749d1f1d7a1e0c0571513e0522b62673c45
Mark Markaryan <markmark206@gmail.com>
2025-08-15 16:41:01 -0700

    adding test examples for testing journey graph and individual 'business logic' functions

---
+++ test/demo/horoscope_graph_test.exs
@@ -0,0 +1,43 @@
+defmodule Demo.HoroscopeGraphTest do
+  use Demo.DataCase
+
+  alias Demo.HoroscopeGraph
+
+  describe "testing graph behavior" do
+    test "Bowser is blocked from the system" do
+      execution =
+        HoroscopeGraph.graph()
+        |> Journey.start_execution()
+        |> Journey.set_value(:name, "Bowser")
+
+      # Wait for the computation to complete and check the result
+      assert Journey.get_value(execution, :name_validation, wait_new: true) ==
+               {:ok, "bad_bad_bad"}
+    end
+  end
+
+  describe "testing 'business logic'" do
+    test "compute_zodiac_sign/1" do
+      assert {:ok, "Capricorn"} =
+               HoroscopeGraph.compute_zodiac_sign(%{birth_month: "January", birth_day: 1})
+
+      assert {:ok, "Aquarius"} =
+               HoroscopeGraph.compute_zodiac_sign(%{birth_month: "January", birth_day: 20})
+
+      assert {:ok, "Pisces"} =
+               HoroscopeGraph.compute_zodiac_sign(%{birth_month: "February", birth_day: 19})
+
+      assert {:ok, "Pisces"} =
+               HoroscopeGraph.compute_zodiac_sign(%{birth_month: "February", birth_day: 20})
+
+      assert {:ok, "Pisces"} =
+               HoroscopeGraph.compute_zodiac_sign(%{birth_month: "March", birth_day: 20})
+
+      assert {:ok, "Aries"} =
+               HoroscopeGraph.compute_zodiac_sign(%{birth_month: "March", birth_day: 21})
+
+      assert {:ok, "Taurus"} =
+               HoroscopeGraph.compute_zodiac_sign(%{birth_month: "April", birth_day: 24})
+    end
+  end
+end
